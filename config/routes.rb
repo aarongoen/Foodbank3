70dd203e4c74ee921685128485381363f7aecc7e
aarongoen <aarongoen2.0@gmail.com>
2021-01-14 16:28:07 -0500

    Creates requests index, new, & show views; Updates layout application format; Creates routes for sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  resources :donors
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Routes for Sessions
+
+  root 'sessions#welcome'
+  get '/signup', to: 'sessions#signup'
+  post '/create', to: 'sessions#create'
+  
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#login'
+
+  post '/logout', to: 'sessions#logout'
+    
+    resources :donors
   resources :requests
   resources :requesters
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
